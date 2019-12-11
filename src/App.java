71a5f9a421c8a9a1141a6ee55e6d468e3b95ab98
Makar-tester <makariv2004@gmail.com>
2019-12-11 19:21:59 +0300

    sad COMMIT!

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String
     }
 }
